c12700b45d8f277627b3cdffbeba72d7e61c8e27
tarikdavis <fearofbuttons@gmail.com>
2016-08-29 16:14:34 +0100

    added delte comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
-  get 'articles/_form'
+  get 'comments/_form'
 
-  get 'articles/edit'
-
-  get 'articles/index'
-
-  get 'articles/show'
+  get 'comments/_comment'
 
   get 'welcome/index'
 
-  resources :articles
+  resources :articles do
+  resources :comments
+  end
+
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
